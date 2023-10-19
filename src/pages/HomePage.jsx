1032927ec7f289764f35850fd4c8227efb72b08f
ervanervan <ervanervan040@gmail.com>
2023-10-19 19:52:39 +0700

    Managed to get data on all products from the API from the FAKE STORE API Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Card from "../components/card";
+import Card from "../components/Card";
 
 const HomePage = () => {
   const url = "https://fakestoreapi.com/products";
