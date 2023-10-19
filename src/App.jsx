f0555074feb86c4fcb8365b33a968bcd050b7dfc
ervanervan <ervanervan040@gmail.com>
2023-10-19 16:26:17 +0700

    Managed to get data on all products from the API from the FAKE STORE API

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import Card from "./components/card";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const url = "https://fakestoreapi.com/products";
-  const [products, setProducts] = useState([]);
-
-  const getDataProducts = async () => {
-    const response = await fetch(url);
-    const dataProduct = await response.json();
-    setProducts(dataProduct);
-  };
-
-  useEffect(() => {
-    getDataProducts();
-  });
-
   return (
     <>
-      <section className="bg-slate-50">
-        <div className="container mx-auto">
-          <h1 className="font-bold text-2xl p-6 md:p-8">Product Cards</h1>
-          <div className="px-6 pb-6 flex gap-6 flex-wrap items-start justify-center">
-            {products.map((product) => (
-              <Card key={product.id} {...product} />
-            ))}
-          </div>
-        </div>
-      </section>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
     </>
   );
 }
