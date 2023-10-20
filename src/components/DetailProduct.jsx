983a2a2cb720c840089a2027438adc398ee14531
ervanervan <ervanervan040@gmail.com>
2023-10-20 23:41:00 +0700

    Managed to get data on all products from the API from the FAKE STORE API add detail product

--- src/components/DetailProduct.jsx
+++ src/components/DetailProduct.jsx
@@ -12,14 +12,14 @@ export default function DetailProduct({
       <div className="container mx-auto">
         <h1 className="font-bold text-2xl p-6 md:p-8">Detail Product</h1>
         <div className="flex gap-10 justify-center items-center">
-          <div className="">
+          <div className="rounded-md shadow-md">
             <img src={image} alt="" />
           </div>
-          <div className="">
-            <h2>{title}</h2>
-            <p>{category}</p>
-            <p>{description}</p>
-            <p>{price}</p>
+          <div className="flex gap-4">
+            <h2 className="font-bold text-2xl">{title}</h2>
+            <p className="italic text-lg">{category}</p>
+            <p className="text-base font-medium">{description}</p>
+            <p className="text-2xl font-bold">{price}</p>
           </div>
         </div>
       </div>
